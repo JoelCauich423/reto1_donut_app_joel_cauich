57bab964dcb8de6b4853243e4bc6fb051e6aa4d4
JoelCauich423 <alu.joelcauich.060.yuc.conalep@gmail.com>
2024-09-05 15:19:51 -0600

    Añadiendo texto principal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,8 +13,34 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu, color: Colors.grey[800]),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: IconButton(icon: Icon(Icons.menu, color: Colors.grey[800], size: 36,), onPressed: () {
+             print('Boton de menú'); },),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person, color: Colors.grey[800], size: 36,),
+          )],
       ),
+      body: const Column(children: [
+        Padding(
+          padding: EdgeInsets.only(left: 24.0),
+          child: Row(
+            children: [
+              Text('I want to ', style: TextStyle(fontSize: 24)),
+              Text('Eat', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,
+                                           decoration: TextDecoration.underline))
+          
+      ]
+          ),
+        )
+        //Tab bar
+
+        //Tab bar  view
+      ],),
+      //Texto: I want to eat 
     );
   }
 }
